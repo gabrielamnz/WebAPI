e65011c11a33830504e43de737536d4bd8490293
gabrielamnz <gmnzfranco@gmail.com>
2019-07-01 20:45:29 -0300

    Projeto funcionando

---
+++ WebAPI/Models/Tarefa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    [Table("Tarefas")]
+    public class Tarefa
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(50,ErrorMessage ="O Título deve ter até 50 caracteres.")]
+        public string Titulo { get; set; }
+
+        [MaxLength(200,ErrorMessage ="A descrição deve ter até 200 caracteres.")]
+        public string Descricao { get; set; }
+    }
+}
